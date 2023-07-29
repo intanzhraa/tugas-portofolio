9df8f47beae2b6ce3ab52a22bbcd01743bf2c8d9
iqbalzeeee <iqbalnauval920@gmail.com>
2023-07-29 11:44:34 +0700

    portofolio

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -11,7 +11,7 @@ import { TheCard } from 'flowbite-vue'
     <table>
         <tr>
             <td>
-                <the-card variant="image" img-src="otann.jpg" img-alt="Desk">
+                <the-card variant="image" img-src="public/otann.jpg" img-alt="Desk">
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Intan Zahra Putri</h5>
     <p class="font-normal text-gray-700 dark:text-gray-400">
         Nama : Intan Zahra Putri 
